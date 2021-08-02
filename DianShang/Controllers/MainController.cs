992f193ee094bd3059f5dbed27998abd3e2b123c
ZhanJifa <2363029513@qq.com>
2021-08-02 15:10:03 +0800

    用户信息

---
+++ DianShang/Controllers/MainController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DianShang.Controllers
+{
+    public class MainController : Controller
+    {
+        // 管理员主页面视图加载
+        public ActionResult Main()
+        {
+            return View();
+        }
+    }
+}
---
+++ DianShang/Controllers/UserController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DianShang.Controllers
+{
+    public class UserController : Controller
+    {
+        Models.DianziShangwuEntities db = new Models.DianziShangwuEntities();
+        //用户信息视图加载
+        public ActionResult UserIndex()
+        {
+            return View();
+        }
+        //
+        public ActionResult SelectUsers(string UserName)
+        {
+            var sel = db.member.ToList();
+            if (UserName!=null)
+            {
+                sel = sel.Where(s => s.name.Contains(UserName) || s.truename.Contains(UserName)).ToList();
+            }
+            return Json(sel, JsonRequestBehavior.AllowGet);
+
+        }
+    }
+}
